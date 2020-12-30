e1c2eacf8ac4c281b8b2d696f55ea1e4db396b39
xixiaohui <yamazake@yeah.net>
2020-12-30 16:38:16 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Welcome to mixsoap.</h1>
+
+    <RegularButtons />
+  </div>
+</template>
+
+<script>
+import RegularButtons from './components/RegularButtons.vue'
+
+export default {
+  name: 'App',
+  components: {
+    RegularButtons
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
